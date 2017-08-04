5ca4ce7a0b812e47ff25ddc944a1518cbaa023c3
xyy601chenyan <xyy545688506@gmail.com>
2017-08-04 10:59:19 +0800

    replace layout on admin/products page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u=User.new
+u.email = 'admin@test.com'
+u.password = '123456'
+u.password_confirmation = '123456'
+u.is_admin = true
+u.save
+
+a = User.new(email:"123@gmail.com",password:"123456",password_confirmation:"123456",is_admin:true)
+a.save
+
+b = User.create(email:"678@gmail.com",password:"123456",password_confirmation:"123456")
+b.is_admin=true
+b.save
+
+c=User.create(email:"000@gmail.com",password:"123456",password_confirmation:"123456")
+
+d=User.new(email:"666@gmail.com",password:"123456",password_confirmation:"123456",is_admin:true)
