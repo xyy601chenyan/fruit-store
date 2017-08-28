ca77d67d82c0611497d230d7dd50cf99e903363c
xyy601chenyan <xyy545688506@gmail.com>
2017-08-28 15:32:34 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validates :billing_name, presence:{message:"请填写订购人姓名"}
+  validates :billing_address,presence:{message:"请填写订购人地址"}
+  validates :shipping_name,presence:{message:"请填写收件人姓名"}
+  validates :shipping_address,presence:{message:"请填写收件人地址"}
 end
