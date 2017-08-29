1ef84711cb767db99379a14f47a860a44a484123
xyy601chenyan <xyy545688506@gmail.com>
2017-08-29 11:51:09 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,14 @@
 class Order < ApplicationRecord
+  before_create :generate_token
   belongs_to :user
-  belongs_to :product_lists
+  has_many :product_lists
   validates :billing_name, presence:{message:"请填写订购人姓名"}
   validates :billing_address,presence:{message:"请填写订购人地址"}
   validates :shipping_name,presence:{message:"请填写收件人姓名"}
   validates :shipping_address,presence:{message:"请填写收件人地址"}
+
+  def generate_token
+      self.token = SecureRandom.uuid
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Store
     # -- all .rb files in that directory are automatically loaded.
     config.action_view.sanitized_allowed_tags = Rails::Html::WhiteListSanitizer.allowed_tags + %w(table tr td)
     config.action_view.sanitized_allowed_attributes = Rails::Html::WhiteListSanitizer.allowed_attributes + %w(style border)
+  
 
   end
 end
