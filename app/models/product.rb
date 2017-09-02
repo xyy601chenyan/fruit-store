6b0b985e1bcf76c345d3c466436f39488e890527
xyy601chenyan <xyy545688506@gmail.com>
2017-09-02 19:58:15 +0800

    only administrator can enter admin layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :title,presence: true
   validates :price, presence: {message:"请填入商品价格"}
+  validates :quantity, presence: {message:"请填入商品数量"}
   mount_uploader :image, ImageUploader
   mount_uploaders :avatars, AvatarUploader
   serialize :avatars, JSON
